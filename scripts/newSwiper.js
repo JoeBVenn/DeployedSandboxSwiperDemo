78c49706caa4a35d676168ee5a0967bf57d92692
Joe Brennan <131769643+JoeBVenn@users.noreply.github.com>
2023-10-12 16:52:43 +0100

    first commit

---
+++ scripts/newSwiper.js
@@ -0,0 +1,236 @@
+
+
+var itemData = [
+	{
+        "id": "1",
+		"title": "Node JS Developer - Remote (Manchester)",
+		"value": "£20,000-£50,000",
+        "type": "Permanent",
+        "location": "Manchester",
+        "description": "Aliquam efficitur mauris quis euismod convallis. Suspendisse potenti. Pellentesque sed euismod dui, sed eleifend orci. Vivamus gravida elit sed condimentum fringilla."
+	},
+	{
+		"id": "2",
+		"title": "Senior Full Stack PHP Developer",
+		"value": "£20,000-£50,000",
+        "type": "Permanent",
+        "location": "Manchester 2",
+        "description": "Aliquam efficitur mauris quis euismod convallis. Suspendisse potenti. Pellentesque sed euismod dui, sed eleifend orci. Vivamus gravida elit sed condimentum fringilla."
+	},
+	{
+		"id": "3",
+		"title": "Backend Software Developer (PHP/Python)",
+		"value": "£20,000-£50,000",
+        "type": "Permanent",
+        "location": "Manchester 3",
+        "description": "Aliquam efficitur mauris quis euismod convallis. Suspendisse potenti. Pellentesque sed euismod dui, sed eleifend orci. Vivamus gravida elit sed condimentum fringilla."
+	},
+	{
+		"id": "4",
+		"title": "Title 4",
+		"value": "£20,000-£50,000",
+        "type": "Permanent",
+        "location": "Manchester 4",
+        "description": "Aliquam efficitur mauris quis euismod convallis. Suspendisse potenti. Pellentesque sed euismod dui, sed eleifend orci. Vivamus gravida elit sed condimentum fringilla."
+	},
+	{
+		"id": "4",
+		"title": "Title 5",
+		"value": "£20,000-£50,000",
+        "type": "Permanent",
+        "location": "Manchester 5",
+        "description": "Aliquam efficitur mauris quis euismod convallis. Suspendisse potenti. Pellentesque sed euismod dui, sed eleifend orci. Vivamus gravida elit sed condimentum fringilla."
+	},
+	{
+		"id": "6",
+		"title": "Title 6",
+		"value": "£20,000-£50,000",
+        "type": "Permanent",
+        "location": "Manchester 6",
+        "description": "Aliquam efficitur mauris quis euismod convallis. Suspendisse potenti. Pellentesque sed euismod dui, sed eleifend orci. Vivamus gravida elit sed condimentum fringilla."
+	},
+	{
+		"id": "7",
+		"title": "Title 7",
+		"value": "£20,000-£50,000",
+        "type": "Permanent",
+        "location": "Manchester 7",
+        "description": "Aliquam efficitur mauris quis euismod convallis. Suspendisse potenti. Pellentesque sed euismod dui, sed eleifend orci. Vivamus gravida elit sed condimentum fringilla."
+	},
+    {
+		"id": "4",
+		"title": "Title 8",
+		"value": "£20,000-£50,000",
+        "type": "Permanent",
+        "location": "Manchester 5",
+        "description": "Aliquam efficitur mauris quis euismod convallis. Suspendisse potenti. Pellentesque sed euismod dui, sed eleifend orci. Vivamus gravida elit sed condimentum fringilla."
+	},
+	{
+		"id": "6",
+		"title": "Title 9",
+		"value": "£20,000-£50,000",
+        "type": "Permanent",
+        "location": "Manchester 6",
+        "description": "Aliquam efficitur mauris quis euismod convallis. Suspendisse potenti. Pellentesque sed euismod dui, sed eleifend orci. Vivamus gravida elit sed condimentum fringilla."
+	},
+	{
+		"id": "7",
+		"title": "Title 10",
+		"value": "£20,000-£50,000",
+        "type": "Permanent",
+        "location": "Manchester 7",
+        "description": "Aliquam efficitur mauris quis euismod convallis. Suspendisse potenti. Pellentesque sed euismod dui, sed eleifend orci. Vivamus gravida elit sed condimentum fringilla."
+	},
+    {
+		"id": "4",
+		"title": "Title 11",
+		"value": "£20,000-£50,000",
+        "type": "Permanent",
+        "location": "Manchester 5",
+        "description": "Aliquam efficitur mauris quis euismod convallis. Suspendisse potenti. Pellentesque sed euismod dui, sed eleifend orci. Vivamus gravida elit sed condimentum fringilla."
+	},
+	{
+		"id": "6",
+		"title": "Title 12",
+		"value": "£20,000-£150,000",
+        "type": "Permanent",
+        "location": "Manchester 6",
+        "description": "Aliquam efficitur mauris quis euismod convallis. Suspendisse potenti. Pellentesque sed euismod dui, sed eleifend orci. Vivamus gravida elit sed condimentum fringilla."
+	},
+	{
+		"id": "7",
+		"title": "Title 13",
+		"value": "£20,000-£50,000",
+        "type": "Permanent",
+        "location": "Manchester 7",
+        "description": "Aliquam efficitur mauris quis euismod convallis. Suspendisse potenti. Pellentesque sed euismod dui, sed eleifend orci. Vivamus gravida elit sed condimentum fringilla."
+	},
+    {
+		"id": "6",
+		"title": "Title 14",
+		"value": "£20,000-£250,000",
+        "type": "Permanent",
+        "location": "Manchester 6",
+        "description": "Aliquam efficitur mauris quis euismod convallis. Suspendisse potenti. Pellentesque sed euismod dui, sed eleifend orci. Vivamus gravida elit sed condimentum fringilla."
+	},
+	{
+		"id": "7",
+		"title": "Title 15",
+		"value": "£20,000-£350,000",
+        "type": "Permanent",
+        "location": "Manchester 7",
+        "description": "Aliquam efficitur mauris quis euismod convallis. Suspendisse potenti. Pellentesque sed euismod dui, sed eleifend orci. Vivamus gravida elit sed condimentum fringilla."
+	},
+    {
+		"id": "4",
+		"title": "Title 16",
+		"value": "£20,000-£50,000",
+        "type": "Permanent",
+        "location": "Manchester 5",
+        "description": "Aliquam efficitur mauris quis euismod convallis. Suspendisse potenti. Pellentesque sed euismod dui, sed eleifend orci. Vivamus gravida elit sed condimentum fringilla."
+	},
+	{
+		"id": "6",
+		"title": "Title 17",
+		"value": "£20,000-£50,000",
+        "type": "Permanent",
+        "location": "Manchester 6",
+        "description": "Aliquam efficitur mauris quis euismod convallis. Suspendisse potenti. Pellentesque sed euismod dui, sed eleifend orci. Vivamus gravida elit sed condimentum fringilla."
+	},
+	{
+		"id": "7",
+		"title": "Title 18",
+		"value": "£18,000",
+        "type": "Permanent",
+        "location": "Manchester 7",
+        "description": "Aliquam efficitur mauris quis euismod convallis. Suspendisse potenti. Pellentesque sed euismod dui, sed eleifend orci. Vivamus gravida elit sed condimentum fringilla."
+	},
+    
+]
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 20,
+    mousewheel: true,
+    
+    grid: {
+        rows: 1,
+    },
+    breakpoints: {
+        
+        768: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+          grid: {
+            rows: 2,
+        },
+        },
+        // when window width is >= 480px
+        1000: {
+          slidesPerView: 3,
+          slidesPerGroup: 3,
+          grid: {
+            rows: 2,
+        },
+        },
+      },
+      autoplay: {
+        delay: 8000,
+        disableOnInteraction: true,
+      },
+      speed: 1000,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+});
+
+
+document.getElementById('swiper-id').innerHTML = itemData.map(item => 
+    `<div class="swiper-slide">
+        <a class="item-link-wrapper" href="#">
+            <p class="title">${item.title}</p>
+            <p class="salary">Salary: ${item.value}</p>
+            <p class="type">Type: ${item.type}</p>
+            <p class="location">Location: ${item.location}</p>
+            <p class="description-data">${item.description}</p>
+            <span class="swiper-link btn btn-dark">
+                <span>
+                    VIEW
+                </span>
+            </span>
+        </a>
+    </div>`
+).join('')
+
+
+// Get all elements with the class name "top-container"
+const topContainers = document.querySelectorAll('.grid-swiper');
+
+// Define a function to limit the number of characters in a <p> tag
+function limitCharacters(element, limit) {
+  const paragraphs = element.querySelectorAll('.description-data');
+
+  paragraphs.forEach((paragraph) => {
+    // getting text content of each p tag
+    const text = paragraph.textContent;
+
+    if (text.length > limit) {
+      paragraph.textContent = text.slice(0, limit) + '...'; // Truncate the text and add "..."
+    }
+  });
+}
+// For every top container
+topContainers.forEach((container) => {
+  // limit vsm..., 200 characters limit for p tags.
+    limitCharacters(container, 140); 
+});
+
+
+  
+
